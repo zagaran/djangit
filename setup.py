4e8f6798602c5ca3e1d02dc3bbd0b4328e147c29
John Wiltshire-Gordon <jwiltshiregordon@gmail.com>
2022-03-16 08:39:58 -0400

    package setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(name="djangit",
+      version="0.1",
+      description="Django-aware git commands",
+      author="JWG",
+      author_email="john@zagaran.com",
+      url="https://github.com/zagaran/djangit.git",
+      packages=find_packages(),
+)
